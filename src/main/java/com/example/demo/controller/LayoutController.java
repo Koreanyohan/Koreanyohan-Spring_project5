d22884ffdb47859d8e7694a7d621abcdbd04b1e8
yohan <ju9580@gmail.com>
2024-01-08 11:27:10 +0900

    final on 8th jan 2024

--- src/main/java/com/example/demo/controller/LayoutController.java
+++ src/main/java/com/example/demo/controller/LayoutController.java
@@ -11,13 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/layout")
 public class LayoutController {
 	
-	@GetMapping("/ex1_layout")  
-	// templates -> layout -> ex_layout.html
-	public void ex1() {
-		
-		
-		
-		
-		
+	@GetMapping({"/ex1_layout", "/ex1_layout2", "/fragment2", "/fragment3", "/exSidebar"})
+	// templates -> layout -> 파일들.html
+	public void ex1() {	
+				
 	}
 }
